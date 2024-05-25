542693a5e09b9d1074f140e2e467b26fa914512f
Sudhansu Sekhar Sahoo <sudhansusahoo359@gmail.com>
2024-05-25 09:18:22 +0530

    added functionality for createBill, editable table

--- src/components/TableContainer/Table.jsx
+++ src/components/TableContainer/Table.jsx
@@ -10,6 +10,71 @@ const Table = (props) => {
     return sumTotal;
   };
 
+  const addBlankRow = props.addBlankRow;
+
+  const handleKeyDown = (event, row, col) => {
+    // console.log("index -> "+row);
+
+    if (event.key === 'ArrowRight') {
+      // console.log('Right arrow key pressed');
+      const nextIndex = col + 1;
+      const nextInput = document.getElementById(`input-${row}-${nextIndex}`);
+      if (nextInput) {
+          nextInput.focus();
+      }
+    }
+    else if (event.key === 'ArrowLeft') {
+      // console.log('Left arrow key pressed');
+      const nextIndex = col - 1;
+      const nextInput = document.getElementById(`input-${row}-${nextIndex}`);
+      if (nextInput) {
+          nextInput.focus();
+      }
+    }
+    else if (event.key === 'ArrowUp') {
+      // console.log('Up arrow key pressed');
+      const nextIndex = row - 1;
+      const nextInput = document.getElementById(`input-${nextIndex}-${col}`);
+      if (nextInput) {
+          nextInput.focus();
+      }
+    }
+    else if (event.key === 'ArrowDown') {
+      // console.log('Down arrow key pressed');
+      // console.log(props.items.length)
+      // console.log(row)
+      if(props.items.length - 1 === row)
+      {
+        addBlankRow();
+      }
+      
+        const nextIndex = row + 1;
+        const nextInput = document.getElementById(`input-${nextIndex}-${col}`);
+        if (nextInput) {
+            nextInput.focus();
+        }
+      
+    }
+  };
+  const updateBillItem = props.updateBillItem;
+
+  const handleInputChange = (e, id, field) => {
+    const newData = props.items.map((item) => {
+      if (item.id === id) {
+          return { ...item, [field]: e.target.value };
+      }
+      return item;
+    });
+
+    updateBillItem(newData);
+  };
+
+
+  const handleDelete = (index) => {
+    const newData = props.items.filter((item, i) => i !== index);
+    updateBillItem(newData);
+};
+
   return (
     <div className="table-container">
       {(
@@ -17,6 +82,7 @@ const Table = (props) => {
             <table className=''>
                 <thead>
                     <tr>
+                        <th></th>
                         <th>Barcode</th>
                         <th>ItemName</th>
                         <th>Price</th>
@@ -27,10 +93,38 @@ const Table = (props) => {
                 <tbody>
                     {props.items.length > 0 && props.items.map((item, index) => (
                         <tr key={index}>
-                            <td>{item.barcode}</td>
-                            <td>{item.itemName}</td>
-                            <td>{item.price}</td>
-                            <td>{item.quantity}</td>
+                            <td>
+                              <button onClick={() => handleDelete(index)}>Delete</button>
+                            </td>
+                            <td>
+                              {item.barcode}
+                            </td>
+                            <td>
+                              <input type="text"
+                                id={`input-${index}-${2}`}
+                                value={item.itemName}
+                                onChange={(e) => handleInputChange(e, index, 'itemName')}
+                                onKeyDown={(e) => handleKeyDown(e, index, 2)}
+                                />
+                            </td>
+                            <td>
+                              <input 
+                                // type="number"
+                                id={`input-${index}-${3}`}
+                                value={item.price}
+                                onChange={(e) => handleInputChange(e, index, 'price')}
+                                onKeyDown={(e) => handleKeyDown(e, index, 3)}
+                                />
+                            </td>
+                            <td>
+                              <input 
+                                // type="number"
+                                id={`input-${index}-${4}`}
+                                value={item.quantity}
+                                onChange={(e) => handleInputChange(e, index, 'quantity')}
+                                onKeyDown={(e) => handleKeyDown(e, index, 4)}
+                                />
+                            </td>
                             <td>{item.price * item.quantity}</td>
                         </tr>
                     ))}
--- src/components/billing/billing.jsx
+++ src/components/billing/billing.jsx
@@ -1,52 +1,58 @@
 import './Billing.css';
 import React, { useRef,useEffect,useState } from 'react';
 import { ReactToPrint } from 'react-to-print';
-import Grid from '../Grid/Grid';
 import Table from '../TableContainer/Table';
+import EditableTable from '../EditableTable/EditableTable';
 
 const Billing = () => {
+  const ipAddress = "http://192.168.0.106:91/"
+  const shopName = 'Nandini Fashion';
+  const address = 'Motto, Chandabali, Bhadrak';
+  const gstNumber = '2347JDKJDGK&56757';
 
-  const [shopName, setShopName] = useState('Nandini Fashion');
-  const [address, setAddress] = useState('Motto, Chandabali, Bhadrak');
-  const [gstNumber, setGstNumber] = useState('2347JDKJDGK&56757');
   const [billNumber, setBillNumber] = useState('12345');
-  const [customerName, setCustomerName] = useState('System');
+  const [customerName, setCustomerName] = useState('Default');
   const componentRef = useRef(null);
   const reactToPrintRef = useRef();
+  const [responseData, setResponseData] = useState(null);
   const [inputValue, setInputValue] = useState('');
   // const [items, setItems] = useState([]);
   const [dbItems, setDbItems] = useState([]);
+  const [billItems, setBillItems] = useState([]);
 
-  const [items, setItems] = useState([]);
-
-  const handleInputChange = (event) => {
+  const onBarcodeTextChange = (event) => {
       setInputValue(event.target.value);
   };
 
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
-      handleClick();
+      onSubmitButtonClick();
     }
   };
-  const handleClick = () => {
+
+  const onSubmitButtonClick = () => {
     if (inputValue.trim() !== '') {
       let dbItem = dbItems.find(x => x.barcode === inputValue)
       // console.log('item -> '+dbItem)
       if(dbItem !== undefined)
       {
-        let currentItemIndex = items.findIndex(x => x.id === dbItem.id)
-        let item = items[currentItemIndex];
+        let currentItemIndex = billItems.findIndex(x => x.itemId === dbItem.id)
+        let item = billItems[currentItemIndex];
+        console.log('item -> '+item)
+
         if(item === undefined)
         {
-          let item = {id: dbItem.id, barcode: dbItem.barcode, itemName: dbItem.itemName, price: dbItem.price, quantity: 1}
-          setItems([...items, item]);
+          const id = billItems.length+1;
+          const item = {"id": id, "itemId": dbItem.id, "barcode": dbItem.barcode, "itemName": dbItem.itemName, 
+            "price": dbItem.price, "quantity": 1, "discountAmount":0}
+          setBillItems([...billItems, item]);
         }
         else
         {
           item.quantity += 1;
-          items[currentItemIndex] = item;
-          console.log(items)
-          setItems([...items])
+          billItems[currentItemIndex] = item;
+          console.log(billItems)
+          setBillItems([...billItems])
         }
 
         setInputValue('');
@@ -60,14 +66,14 @@ const Billing = () => {
 
   const getTotalSum = () => {
     let sumTotal = 0;
-    items.forEach(x => sumTotal += x.price * x.quantity);
+    billItems.forEach(x => sumTotal += x.price * x.quantity);
 
     return sumTotal;
   };
 
   const handleCtrlP = () => {
-    console.log('Ctrl + P was pressed');
-    alert('Ctrl + P was pressed');
+    // console.log('Ctrl + P was pressed');
+    // alert('Ctrl + P was pressed');
     handlePrint();
   };
 
@@ -88,12 +94,12 @@ const Billing = () => {
     window.addEventListener('keydown', handleKeyDown);
 
     return () => {
-        window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
 
   useEffect(() => {
-    fetch('http://192.168.0.106:91/item') // Replace with your API endpoint
+    fetch(ipAddress+'item') // Replace with your API endpoint
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -103,7 +109,7 @@ const Billing = () => {
       .then(data => {
         // setData(data);
         // setLoading(false);
-        console.log(data)
+        // console.log(data)
         setDbItems(data);
       })
       .catch(error => {
@@ -120,16 +126,108 @@ const Billing = () => {
       });
   }, []);
 
+  const updateBillItem = (updatedBillItem) => {
+    setBillItems(updatedBillItem)
+  }
+
+  const addBlankRow = () => {
+    const id = billItem.length + 1;
+    const billItem = {"id": id, 'itemId': 0, "barcode": '', "itemName": '', 
+    "price": 0, "quantity": 1, "discountAmount":0}
+  setBillItems([...billItems, billItem]);
+  }
+
+  const handleSubmit = async (bill) => {
+      // event.preventDefault();
+      // const postData = {
+      //     name: 'John Doe',
+      //     email: 'johndoe@example.com'
+      // };
+
+      const body = JSON.stringify(bill);
+      console.log(body)
+
+      try {
+          const response = await fetch(ipAddress+'api/Bill', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(bill)
+          });
+          console.log(response)
+          // if(response.status)
+          const data = await response.json();
+          //handlePrint();
+          setResponseData(data);
+      } catch (error) {
+          console.error('Error:', error);
+          alert(error)
+      }
+  };
+
+  const createBill = (billItemList, discountAmount, modeOfPayment, customerName, customerAddress, isPrintBillEnable) => {
+    try
+    {
+    const billItemDto = [];
+    billItemList.forEach(ele => {
+      billItemDto.push({
+        "id": ele.id,
+        "itemId": ele.itemId,
+        "barcode": ele.barcode,
+        "itemName": ele.itemName,
+        "quantity": ele.quantity,
+        "price": ele.price,
+        "discountAmount": ele.discountAmount,
+        "amount": ele.quantity * ele.price - ele.discountAmount,
+      });
+    });
+
+    let gst = 0;
+    let subTotal = 0;
+    let totalAmount = 0;
+    billItemDto.forEach(x => subTotal += x.amount);
+    totalAmount = subTotal - discountAmount + gst;
+    const bill = {
+      "id": 0,
+      "subTotal": subTotal,
+      "discountAmount": discountAmount,
+      "totalAmount": totalAmount,
+      "modeOfPayment": modeOfPayment,
+      "customerName": customerName,
+      "customerAddress": customerAddress,
+      "billItems": billItemDto,
+    }
+    
+    console.log('handleSubmit')
+  
+    // POST request
+    handleSubmit(bill)
+
+    // on success response
+    if(isPrintBillEnable)
+    {
+      handlePrint();
+    }
+  }catch(e)
+  {
+    console.log(e)
+  }
+
+  }
+
   return (
     <div>
         <div>
       <input type="text"
         value={inputValue}
-        onChange={handleInputChange}
+        onChange={onBarcodeTextChange}
         onKeyDown={handleKeyDown}
         placeholder="Enter barcode"
       />
-      <button onClick={handleClick}>Submit</button>
+      <button onClick={onSubmitButtonClick}>Submit</button>
+      <button onClick={onSubmitButtonClick}>Clear</button>
+      <button onClick={() => createBill(billItems, 0, 'cash', 'default', '', false)}>Create Bill</button>
       <ReactToPrint
           trigger={() => {
             return <button>Print Bill</button>;
@@ -138,17 +236,17 @@ const Billing = () => {
           ref={reactToPrintRef}
         />
       
-      <Table items={items}/>
+      <Table items={billItems} updateBillItem={updateBillItem} addBlankRow={addBlankRow}/>
+      {/* <EditableTable /> */}
     </div>
 
       {/* print */}
       <div className='print-area'>
-        
         <div ref={componentRef}>
-          <div style={{fontSize:'20px', margin:'0px'}}>{shopName}</div>
-          <div style={{fontSize:'15px', margin:'0px'}}>{address}</div>
-          <div style={{fontSize:'15px', margin:'0px'}}>GSTIN: {gstNumber}</div>
-          <div style={{fontSize:'20px', margin:'0px'}}>Retail Invoice</div>
+          <div style={{fontSize:'20px', margin:'0px', textAlign: 'center'}}>{shopName}</div>
+          <div style={{fontSize:'15px', margin:'0px', textAlign: 'center'}}>{address}</div>
+          <div style={{fontSize:'15px', margin:'0px', textAlign: 'center'}}>GSTIN: {gstNumber}</div>
+          <div style={{fontSize:'20px', margin:'0px', textAlign: 'center'}}>Retail Invoice</div>
           <div style={{fontSize:'12px', margin:'0px'}}>Bill Number: {billNumber}</div>
           <div style={{fontSize:'12px', margin:'0px'}}>Date: {billNumber}</div>
           <div style={{fontSize:'12px', margin:'0px'}}>Name: {customerName}</div>
@@ -163,7 +261,7 @@ const Billing = () => {
                 </tr>
             </thead>
             <tbody>
-                {items.length > 0 && items.map((item, index) => (
+                {billItems.length > 0 && billItems.map((item, index) => (
                     <tr key={index}>
                         <td>{item.barcode}</td>
                         <td>{item.itemName}</td>
@@ -179,8 +277,8 @@ const Billing = () => {
         <div style={{fontSize:'12px', margin:'0px'}}>Total Amount {getTotalSum() * 1.05}</div>
         <div>------------------------------------------------</div>
         <div style={{fontSize:'12px', margin:'0px'}}>Total {getTotalSum()}</div>
-        <div style={{fontSize:'12px', margin:'0px'}}>CGST 2.5%{getTotalSum() * 0.025}</div>
-        <div style={{fontSize:'12px', margin:'0px'}}>SGST 2.5%{getTotalSum() * 1.025}</div>
+        <div style={{fontSize:'12px', margin:'0px'}}>CGST 2.5% {getTotalSum() * 0.025}</div>
+        <div style={{fontSize:'12px', margin:'0px'}}>SGST 2.5% {getTotalSum() * 1.025}</div>
         <div>------------------------------------------------</div>
         <div style={{fontSize:'12px', margin:'0px'}}>Terms & Conditions:</div>
         <div style={{fontSize:'12px', margin:'0px'}}>Subject to {shopName} No cash return No gurantee on the products</div>
