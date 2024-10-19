cec09c6e6a5ed8fd8365aaa05393356529def2d6
SudhansuSekharSahoo18 <sudhansusahoo359@gmail.com>
2024-10-19 20:05:56 +0530

    added readonly in customInput

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './CustomInput.css';
 
-const CustomInput = ({ label, text, type, setText }) => {
+const CustomInput = ({ label, text, type, setText, isReadyOnly }) => {
     return (
         <div className='customInput'>
             <label>{label} </label>
-            <input id={label} type={type} value={text}
+            <input id={label} type={type} value={text} readOnly={isReadyOnly}
                 onChange={(e) => setText(type === 'number' ? parseInt(e.target.value) : e.target.value)} />
         </div>
     );
